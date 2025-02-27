3283abebd7683d85b9d3399916bfad8ef723292d
icaro <152525227+icaroo01@users.noreply.github.com>
2025-02-27 19:53:34 -0300

    Add files via upload

---
+++ aula2.js
@@ -0,0 +1,14 @@
+
+
+function teste(){
+    nome = document.getElementById('nome').value
+    alert('olá '+nome)
+
+}
+
+function somar(){
+   n1 = parseInt(document.getElementById('n1').value)
+   n2 = parseInt(document.getElementById('n2').value)
+   soma = n1+n2
+   alert ('a soma deu '+soma)
+}
